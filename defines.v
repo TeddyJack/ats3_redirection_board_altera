4c8ef4f770ec4e232d08ea337588e8b6c6d4fc3c
Maximenkov <fapdayz@gmail.com>
2017-09-21 14:50:49 +0300

    Fixed output uart module error when second byte was missing in odd-num message after even-num message

--- defines.v
+++ defines.v
@@ -2,8 +2,8 @@
 `define NUM_UART			1
 `define PREFIX				16'h44BB
 `define F_clk				48000000	// Make sure that in PLL the same value
-`define PKTEND_PERIOD	1000		// in ms
-`define GFM_PERIOD		500		// in ms (GFM = got full message), time period of triggering GOT_FULL_MESSAGE if input FIFO is not full enough
+`define PKTEND_PERIOD	5			// in ms
+`define GFM_PERIOD		1			// in ms (GFM = got full message), time period of triggering GOT_FULL_MESSAGE if input FIFO is not full enough
 `define UART_WORDS		32			// how many words used in FIFO to trigger GOT_FULL_MESSAGE
 `define SPI_WORDS			256
 `define NUM_LEDS			3
